434ea44b1487065ff785886efc6fc4b6c8cf6d11
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2024-05-03 14:27:21 -0300

    Aprende mais sobre parametros com useState

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Nome from './components/Nome';
 function App() {
   const [aluno, setAluno] = useState('Sujeito Programador')
 
+  function handleChangeName(name) {
+    setAluno(name)
+  }
+
   return (
     <div >
       <h1>Componentes App</h1>
       <h2>Olá: {aluno} </h2>
-      <Nome aluno= "Pedro Moura" idade={33}/>
+      <button onClick={ () => handleChangeName('Pedro Moura') }>
+        Muda Nome
+      </button>
     </div>
   );
 }
