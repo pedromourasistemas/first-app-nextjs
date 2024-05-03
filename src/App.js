3d8720f3ee3e59168fa23cdd97746c74d3c3212d
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2024-05-03 16:28:40 -0300

    Manipulando listas

--- src/App.js
+++ src/App.js
@@ -2,47 +2,36 @@ import { useState } from 'react';
 
 function App() {
 
-  const[nome, setNome] = useState('');
-  const[email, setEmail] = useState('');
-  const[idade, setIdade] = useState(0);
-
-  const[user, setUser] = useState({});
+  const[tarefas, setTarefas] = useState([
+    'Pagar a conta de Luz',
+    'Estudar React JS'
+  ]);
+  const[input, setInput] = useState('');
 
   function handleRegister(e) {
     e.preventDefault()
 
-    alert('Usuário registrado com sucesso!')
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
   
   return (
     <div >
       <h1>Cadastrando Usuário</h1>
       <form onSubmit={handleRegister}>
-        <label>Nome:</label><br/>
-        <input placeholder='Digite seu nome' value={nome} onChange={ (e) => setNome(e.target.value) } /><br/>
-
-        <label>E-mail:</label><br/>
-        <input placeholder='Digite seu e-mail' value={email} onChange={ (e) => setEmail(e.target.value) } /><br/>
-
-        <label>Idade:</label><br/>
-        <input placeholder='Digite sua idade' value={idade} onChange={ (e) => setIdade(e.target.value) } /><br/>
+        <label>Nome da tarefa:</label><br/>
+        <input placeholder='Digite uma tarefa' value={input} onChange={ (e) => setInput(e.target.value) } /><br/>
 
         <button type='submit'>Registrar</button>
       </form>
 
       <br/><br/>
 
-      <div>
-        <span>Bem vindo: {user.nome}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>E-mail: {user.email}</span><br/>
-      </div>
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={ tarefa }>{ tarefa }</li>
+        ))}
+      </ul>
     </div>
   );
 }
